b7fb8605dcd159068bc4d26e860f98e39014d3c5
Carmen Bravo <carmen.bravogonzalezblas@kuleuven.vib.be>
2019-04-09 19:59:51 +0200

    Correct warning in createcisTopicObjectFrom10X

--- R/InitializecisTopic.R
+++ R/InitializecisTopic.R
@@ -171,7 +171,7 @@ createcisTopicObjectFrom10X <- function(
   barcodes <- rownames(CR_metrics)
   
   # Select fragments from good cells
-  good_fragments <- data.table::fread(paste0("zcat < ", frag_gz_file)) %>% 
+  good_fragments <- data.table::fread(cmd=paste0("zcat < ", frag_gz_file)) %>% 
     data.frame() %>% filter(V4 %in% barcodes) %>%  # filter for barcodes in our search set
     GenomicRanges::makeGRangesFromDataFrame(seqnames.field = "V1", start.field = "V2", end.field = "V3", keep.extra.columns = TRUE)
   
