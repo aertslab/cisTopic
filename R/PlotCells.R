eb9e311ad54fc65254c5b02d3d7b3fb87d185267
Carmen Bravo <carmen.bravogonzalezblas@kuleuven.vib.be>
2019-11-27 10:35:12 +0100

    Fix color space

--- R/PlotCells.R
+++ R/PlotCells.R
@@ -622,7 +622,11 @@ plotFeatures <- function(
 
 .distinctColorPalette <-function(k) {
   set.seed(123)
-  ColorSpace <<- t(unique(col2rgb(scales::hue_pal(l=60:100)(2e3))))
+  if(packageVersion("scales") >= '1.1.0'){
+    ColorSpace <- t(unique(col2rgb(scales::hue_pal(l=85)(2e3))))
+  } else {
+    ColorSpace <- t(unique(col2rgb(scales::hue_pal(l=60:100)(2e3))))
+  }
   km <- kmeans(ColorSpace, k, iter.max=20)
   colors <- rgb(round(km$centers), maxColorValue=255)
   return(colors)
